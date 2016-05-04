45cd78a89513e52cb5523c46fad0c7b4062bf8be
Melnyk Roman <orakylik@ukr.net>
2016-05-04 13:04:54 +0300

    T749 mail for amazon

---
+++ src/main/java/com/terrasystems/emedics/controllers/ReferenceController.java
@@ -0,0 +1,10 @@
+package com.terrasystems.emedics.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/rest/private/reference")
+public class ReferenceController {
+}
---
+++ src/main/java/com/terrasystems/emedics/dao/ReferenceRepository.java
@@ -0,0 +1,8 @@
+package com.terrasystems.emedics.dao;
+
+
+import com.terrasystems.emedics.model.Reference;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReferenceRepository extends CrudRepository<Reference, String>{
+}
