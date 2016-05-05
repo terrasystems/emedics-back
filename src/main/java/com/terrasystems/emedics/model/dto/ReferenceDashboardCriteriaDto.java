6a130d8274587a6fe853ecf437fbd570e4678cca
Melnyk Roman <orakylik@ukr.net>
2016-05-05 12:29:49 +0300

    T751 getAll, edit added

---
+++ src/main/java/com/terrasystems/emedics/model/dto/ReferenceDashboardCriteriaDto.java
@@ -0,0 +1,11 @@
+package com.terrasystems.emedics.model.dto;
+
+
+import java.io.Serializable;
+
+public class ReferenceDashboardCriteriaDto implements Serializable{
+    private static final long serialVersionUID = 1695478627620444793L;
+
+    public ReferenceDashboardCriteriaDto() {
+    }
+}
