27bceffc8396ff14e15c61c305170b129000c240
Fozqu <serhiimorunov@gmail.com>
2016-05-20 17:42:51 +0300

    T789 Activation link fixed

--- src/main/java/com/terrasystems/emedics/security/RegistrationServiceImp.java
+++ src/main/java/com/terrasystems/emedics/security/RegistrationServiceImp.java
@@ -198,7 +198,8 @@ public class RegistrationServiceImp implements RegistrationService {
         RegisterResponseDto response = new RegisterResponseDto();
         StateDto state = new StateDto(true, "User Activated");
         UserDto userDto = new UserDto(user.getEmail(), user.getUsername());
-        userDto.setType("patient");
+        String[] type = token.split(":");
+        userDto.setType(type[2]);
         response.setState(state);
         response.setToken(token);
         response.setUser(userDto);
