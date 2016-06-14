1b735a515ed7136fb41abfc67c287927d6222311
Fozqu <serhiimorunov@gmail.com>
2016-06-14 16:17:23 +0300

    T842 some refactoring

--- src/main/java/com/terrasystems/emedics/services/EventPatientService.java
+++ src/main/java/com/terrasystems/emedics/services/EventPatientService.java
@@ -2,6 +2,7 @@ package com.terrasystems.emedics.services;
 
 
 import com.terrasystems.emedics.model.dto.PatientDto;
+import com.terrasystems.emedics.model.dto.StateDto;
 import com.terrasystems.emedics.model.dto.TemplateEventDto;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 public interface EventPatientService {
     List<PatientDto> getAllPatients();
     List<TemplateEventDto> getPatientsEvents(String patientId);
+    List <PatientDto> findPatientByCriteria(String search);
+    StateDto removePatient(String id);
+
 
 }
