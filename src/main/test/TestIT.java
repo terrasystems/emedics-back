1e90521062d3ea05f6695efaa65b16801b9ca651
Fozqu <serhiimorunov@gmail.com>
2016-06-23 19:00:18 +0300

    test

---
+++ src/main/test/TestIT.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:testContext.xml"})
+@WebAppConfiguration
+public class TestIT {
+
+
+
+
+    @Test
+    public void dbTest() {
+
+    }
+
+}
